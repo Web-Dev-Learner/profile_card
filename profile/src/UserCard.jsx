b11b969f85fd7c652e0fdb5814f4ba3555af323f
Web-Dev-Learner <shribabe2@gmail.com>
2024-06-29 12:53:17 +0530

    updated

--- profile/src/UserCard.jsx
+++ profile/src/UserCard.jsx
@@ -1,11 +1,56 @@
 // import React from 'react';
-function User(){
+import PropTypes from "prop-types";
+const userData=[
+    {
+        name:"Aarna" ,
+        city:"New York",
+        description:"front-end developer",
+
+        skills:["HTML","CSS","Javascript",
+            "React","Node","UI/UX"],
+            online: false,
+            profile:"images/pics1n.jpg",
+
+
+
+    },
+    {
+        name: "Rohan",
+        city: "San Francisco",
+        description: "back-end developer",
+        skills: ["Node.js", "Express", "MongoDB", "Docker", "AWS"],
+        online: true,
+        profile:"images/pics3n.jpg",
+    },
+    {
+        name: "Meera",
+        city: "Chicago",
+        description: "full-stack developer",
+        skills: ["JavaScript", "React", "Node.js", "GraphQL", "PostgreSQL"],
+        online: true,
+        profile:"images/pics2.jpg",
+    },
+    {
+        name: "Vikram",
+        city: "Los Angeles",
+        description: "mobile app developer",
+        skills: ["Java", "Kotlin", "Swift", "React Native", "Firebase"],
+        online: false,
+        profile:"images/picss4.jpeg",
+    }
+
+
+];
+
+function User(props){
 return <div className="card-container">
-    <span className="pro offline">OFFLINE</span>
-    <img src="images/pics1.jpg" className="img" alt="user" />
-    <h3>Full Name</h3>
-    <h3>chennai</h3>
-    <p>front-end developer</p>
+    <span className={props.online?"pro online" : 
+        "pro offline"}>
+            {props.online?"ONLINE":"OFFLINE"}</span>
+    <img src={props.profile} className="img" alt="user" />
+    <h3>{props.name}</h3>
+    <h3>{props.city}</h3>
+    <p>{props.description}</p>
     <div classNmae="buttons">
         <button className="primary">Message</button>
         <button className="primary outline">Following</button>
@@ -13,12 +58,9 @@ return <div className="card-container">
     <div className="skills">
         <h6>Skills</h6>
         <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>
-            JavaScript library</li>
-            <li>RESTful APIs</li>
-            <li>Node</li>
+            {props.skills.map((skill,index)=>(
+                <li key={index}>{skill}</li>
+            ))}
         </ul>
     </div>
     </div>;
@@ -28,7 +70,35 @@ return <div className="card-container">
 export const UserCard = () => {
   return (
     
-        <User />
+      <>
+      {
+        userData.map((user,index) =>(
+            <User key={index}
+            name={user.name}
+            city={user.city}
+            description={user.description}
+            online={user.online}
+            profile={user.profile}
+            skills={user.skills}
+            />
+        ))
+      }
+      </>
         
   )
+}
+/* <User name="Aarna" city="New York"
+        description="front-end developer"
+        skills={["HTML","CSS","Javascript",
+            "React,Node","UI/UX"]} online={false}
+            profile ="images/pics1.jpg"/>
+        */ 
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    city:PropTypes.string.isRequired,
+    description:PropTypes.string.isRequired,
+    skills:PropTypes.arrayOf(PropTypes.string).isRequired,
+    online:PropTypes.bool.isRequired,
+    profile:PropTypes.string.isRequired,
+
 }
